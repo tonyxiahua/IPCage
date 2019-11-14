4a9087e8333237b27217de12fb499e658db97452
tonyxiahua <rednik111@gmail.com>
2019-11-14 01:12:14 +0000

    finished iptables.py

--- iptables.py
+++ iptables.py
@@ -1,40 +1,47 @@
 import subprocess
 import datetime
+import time
+import sys
 import csv
 import collections
 import json
 import os
 
+dirpath = os.getcwd()
 '''
 Read from the csv/json file to check which ip we need to ban from
 Banning the ip fail connects more than 5 times.
 '''
 def checker():
-    with open('/data/ipdb.json') as json_file:
+    BANNING=[]
+    with open(dirpath+'/data/ipdb.json') as json_file:
         jsondic = json.load(json_file)
-        
-        if IP-ADDRESS[1] > 5:
-            BANNING-IP-ADDRESS = IPADDRESS[0]
-        return BANNING-IP-ADDRESS
+        ipList = jsondic.keys()
+        for elem in ipList:
+            if jsondic[elem] > 5:
+                BANNING.append(elem)
+    return BANNING
 
 '''
 Execute command from reading and ban the IP
 This progam should be reguarly running to keep updated
-'''
-def command(IPADDRESS):
-    cmd1= 'iptables -A INPUT -s '+ IP-ADDRESS + ' -j DROP'
-    cmd3= ' '
-    cmd4= ' '
-    os.system(cmd1)
-    os.system()
-    os.system()
-    os.system()
-'''
 Main program also print out the banned ip.
 '''
-def main:
-    checker()
-    command()
+def main():
+    toBeBan = checker()
+    #Clear the empty string
+    while("" in toBeBan) : 
+        toBeBan.remove("") 
+
+    print("Welcome to system iptable banner")
+    for elem in toBeBan:
+        cmd1= 'iptables -A INPUT -s '+ elem + ' -j DROP'
+        os.system(cmd1)
+        text = str(toBeBan.index(elem))+'of'+str(len(toBeBan))
+        sys.stdout.write(text+"\r")
+        sys.stdout.flush()
+    sys.stdout.write("]\n")
+    print("Operation ends.")
 
 if __name__ == "__main__":
     main()
